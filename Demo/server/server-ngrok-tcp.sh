9413c9c6ed1e0b6d08c82d5694493e5b3761765c
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-06-23 09:05:21 +0530

    Update server-ngrok-tcp.sh

--- Demo/server/server-ngrok-tcp.sh
+++ Demo/server/server-ngrok-tcp.sh
@@ -34,7 +34,9 @@ echo -e "[${Green}✔${clear}] ${IYellow} Copy paste utility loading... ${clear}
 $HOME/ngrok tcp 5576 > /dev/null &
 sleep 7
 #ip=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|sed 's#tcp://##g'|sed 's#:# #g'|awk '{print $1}')
-ip=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|sed 's#tcp://##g'|sed 's#:# #g'|awk '{print "ping -c1 " $1}'|sh|grep -oP 'PING.*?\(\K[^)]+')
+#ip=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|sed 's#tcp://##g'|sed 's#:# #g'|awk '{print "ping -c1 " $1}'|sh|grep -oP 'PING.*?\(\K[^)]+')
+ipo=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|sed 's#tcp://##g'|sed 's#:# #g'|awk '{print $1}')
+ip=$(host $ipo|awk '{print $NF}')
 vport=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|sed 's#tcp://##g'|sed 's#:# #g'|awk '{print $2}')
 
 echo -e "
