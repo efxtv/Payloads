d42a78ff3cd1d542523a9d0eb2ab27f95584e12f
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2023-06-23 16:04:21 +0530

    Update ubuntu-in-termux.sh

--- Demo/ubuntu-in-termux.sh
+++ Demo/ubuntu-in-termux.sh
@@ -4,8 +4,8 @@ apt-get install wget -y
 apt-get install proot -y
 apt-get install git -y
 cd ~
-git clone https://github.com/MFDGaming/ubuntu-in-termux.git ~./ubuntu-in-termux-efx
-cd ~./ubuntu-in-termux-efx
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git .ubuntu-in-termux-efx
+cd .ubuntu-in-termux-efx
 rm README.md
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
